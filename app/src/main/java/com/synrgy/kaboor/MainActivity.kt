8304ab1597ba2de8515c0d0f6d0f440b539e8a69
Agung Kusuma <agungoop123@gmail.com>
2023-12-29 21:55:56 +0700

    add: add handling click listener bottom navigation

--- app/src/main/java/com/synrgy/kaboor/MainActivity.kt
+++ app/src/main/java/com/synrgy/kaboor/MainActivity.kt
@@ -2,10 +2,12 @@ package com.synrgy.kaboor
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.synrgy.common.presentation.KaboorActivity
 import com.synrgy.kaboor.databinding.ActivityMainBinding
+import com.wahidabd.library.utils.exts.gone
+import com.wahidabd.library.utils.exts.visible
 
 class MainActivity : KaboorActivity<ActivityMainBinding>() {
 
@@ -23,7 +25,31 @@ class MainActivity : KaboorActivity<ActivityMainBinding>() {
     override fun initIntent() {}
 
     // TODO: For UI
-    override fun initUI() {}
+    override fun initUI() {
+        with(binding) {
+            val navHost =
+                supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+            val navController = navHost.navController
+//            bottomNav.setupWithNavController(navController)
+
+            bottomNav.setOnItemSelectedListener { item ->
+                if (item.itemId != bottomNav.selectedItemId) {
+                    NavigationUI.onNavDestinationSelected(item, navController)
+                }
+                true
+            }
+
+            navController.addOnDestinationChangedListener { _, dest, _ ->
+                when (dest.id) {
+                    R.id.homeFragment,
+                    R.id.orderFragment,
+                    R.id.notificationFragment,
+                    R.id.accountFragment -> bottomNav.visible()
+                    else -> bottomNav.gone()
+                }
+            }
+        }
+    }
 
     // TODO: For Action (Click, Touch, etc)
     override fun initAction() {}
