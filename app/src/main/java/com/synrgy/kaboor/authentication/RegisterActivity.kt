95ffb7d849ac97ab889af13d36b45235377a6d59
muhammadkelvin <muhkelvin36@gmail.com>
2024-01-06 00:53:10 +0700

    implement kondition format email , bottom sheet, validati same email, and slicing bottom sheet detail penumpang dan pemesan

--- app/src/main/java/com/synrgy/kaboor/authentication/RegisterActivity.kt
+++ app/src/main/java/com/synrgy/kaboor/authentication/RegisterActivity.kt
@@ -3,10 +3,14 @@ package com.synrgy.kaboor.authentication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.synrgy.common.presentation.KaboorActivity
 import com.synrgy.kaboor.R
 import com.synrgy.kaboor.databinding.ActivityRegisterBinding
+import com.synrgy.kaboor.databinding.BottomSheetLayoutBinding
 import com.synrgy.kaboor.databinding.DialogErrorBinding
 
 class RegisterActivity : KaboorActivity<ActivityRegisterBinding>() {
@@ -30,7 +34,10 @@ class RegisterActivity : KaboorActivity<ActivityRegisterBinding>() {
         binding.btDaftar.setOnClickListener {
             val email = binding.etEmail.text.toString()
 
-            if (isValidEmail(email)) {
+            if (email == "muhkelvin@gmail.com") {
+                // Menampilkan dialog pesan jika email adalah "muhkelvin@gmail.com"
+                tampilkanDialogPesan("Email ini terdaftar, silahkan gunakan email lain.")
+            } else if (isValidEmail(email)) {
                 // Format email valid, lanjutkan dengan mengirim data
                 kirimData(email)
             } else {
@@ -50,19 +57,48 @@ class RegisterActivity : KaboorActivity<ActivityRegisterBinding>() {
     // TODO: For Observer (LiveData, etc)
     override fun initObservers() {}
 
-    private fun tampilkanDialogError(pesan: String) {
+    fun tampilkanDialogError(pesan: String) {
         val dialogBinding = DialogErrorBinding.inflate(layoutInflater)
-        dialogBinding.tvErrorMessage.text = pesan
+        val tvErrorMessage: TextView = dialogBinding.tvErrorMessage
+        val btnOK: Button = dialogBinding.btnOK
+
+        tvErrorMessage.text = pesan
 
         val dialog = AlertDialog.Builder(this)
             .setView(dialogBinding.root)
             .create()
 
-        dialogBinding.btnOK.setOnClickListener {
+        btnOK.setOnClickListener {
             dialog.dismiss()
         }
 
         dialog.show()
+    }
+
+    private fun tampilkanDialogPesan(pesan: String) {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetBinding = BottomSheetLayoutBinding.inflate(layoutInflater)
+        bottomSheetDialog.setContentView(bottomSheetBinding.root)
+
+        bottomSheetBinding.tvText.text = pesan
+
+        // Setel listener untuk button "Nanti Saja"
+        bottomSheetBinding.btNanti.setOnClickListener {
+            // Menutup BottomSheetDialog
+            bottomSheetDialog.dismiss()
+        }
+
+        // Setel listener untuk button "Login"
+        bottomSheetBinding.btLogin.setOnClickListener {
+            // Menutup BottomSheetDialog
+            bottomSheetDialog.dismiss()
+
+            // Memulai aktivitas login
+//            val intent = Intent(this, DetailRegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        bottomSheetDialog.show()
     }
 
     private fun kirimData(email: String) {
