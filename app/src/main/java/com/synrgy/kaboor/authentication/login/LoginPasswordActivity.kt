40d7216841a231293c4fb56f7494ff192b5fe811
Agung Kusuma <agungoop123@gmail.com>
2024-01-18 14:56:59 +0700

    feat (login-password): Implement API on login password page

--- app/src/main/java/com/synrgy/kaboor/authentication/login/LoginPasswordActivity.kt
+++ app/src/main/java/com/synrgy/kaboor/authentication/login/LoginPasswordActivity.kt
@@ -2,14 +2,25 @@ package com.synrgy.kaboor.authentication.login
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import com.synrgy.common.presentation.KaboorActivity
+import com.synrgy.common.presentation.KaboorPassiveActivity
 import com.synrgy.common.utils.ext.onBackPress
+import com.synrgy.common.utils.ext.removeErrorTextPadding
+import com.synrgy.common.utils.ext.setClearPaddingTextInput
+import com.synrgy.domain.auth.model.request.LoginParam
+import com.synrgy.kaboor.R
+import com.synrgy.kaboor.authentication.AuthViewModel
 import com.synrgy.kaboor.databinding.ActivityLoginPasswordBinding
+import com.synrgy.kaboor.ticket.plane.FlightScheduleActivity
 import com.wahidabd.library.utils.common.emptyString
+import com.wahidabd.library.utils.exts.observerLiveData
+import com.wahidabd.library.utils.exts.onClick
+import com.wahidabd.library.validation.Validation
+import com.wahidabd.library.validation.util.notEmptyRule
+import org.koin.android.ext.android.inject
 
-class LoginPasswordActivity : KaboorActivity<ActivityLoginPasswordBinding>() {
+class LoginPasswordActivity : KaboorPassiveActivity<ActivityLoginPasswordBinding>() {
 
+    private val viewModel: AuthViewModel by inject()
     private var email: String? = emptyString()
 
     companion object {
@@ -26,6 +37,7 @@ class LoginPasswordActivity : KaboorActivity<ActivityLoginPasswordBinding>() {
         ActivityLoginPasswordBinding.inflate(layoutInflater)
 
     override fun initIntent() {
+        super.initIntent()
         email = intent.getStringExtra(EXTRA_EMAIL)
     }
 
@@ -35,6 +47,58 @@ class LoginPasswordActivity : KaboorActivity<ActivityLoginPasswordBinding>() {
 
     override fun initAction() = with(binding) {
         appbar.setOnBackClickListener { onBackPress() }
+        btnLogin.onClick {
+            validate()
+        }
     }
 
+    override fun initObservers() {
+        super.initObservers()
+
+        viewModel.jwt.observerLiveData(
+            this,
+            onLoading = {
+                showLoading()
+            },
+            onFailure = { _, message ->
+                showErrorDialog(message.toString())
+            },
+            onSuccess = {
+                hideLoading()
+                FlightScheduleActivity.start(this)
+                finish()
+            }
+        )
+    }
+
+    override fun onValidationSuccess() = with(binding) {
+        setClearPaddingTextInput(
+            listOf(
+                etEmail.textInput,
+                etPassword.textInput,
+            )
+        )
+        removeErrorTextPadding(
+            listOf(
+                etEmail.textInput,
+                etPassword.textInput,
+            )
+        )
+
+        val data = LoginParam(
+            email = email.toString(),
+            password = etPassword.getText()
+        )
+        viewModel.login(data)
+    }
+
+    override fun setupValidation() = with(binding) {
+        addValidation(
+            Validation(
+                etPassword.textInput, listOf(
+                    notEmptyRule(getString(R.string.error_required_password))
+                )
+            )
+        )
+    }
 }
--- data/src/main/java/com/synrgy/data/user/local/KaboorDataStore.kt
+++ data/src/main/java/com/synrgy/data/user/local/KaboorDataStore.kt
@@ -1,13 +1,13 @@
 package com.synrgy.data.user.local
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.synrgy.common.utils.constant.ConstDataStore
 import com.synrgy.data.user.model.request.UserRequest
 import com.synrgy.data.user.model.response.UserDataResponse
-import com.synrgy.data.user.model.response.UserResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
@@ -27,6 +27,7 @@ class KaboorDataStore(context: Context) {
         private val FULL_NAME = stringPreferencesKey(ConstDataStore.PREF_FULL_NAME)
         private val EMAIL = stringPreferencesKey(ConstDataStore.PREF_EMAIL)
         private val PHONE = stringPreferencesKey(ConstDataStore.PREF_PHONE)
+        private val STATE = booleanPreferencesKey(ConstDataStore.PREF_STATE)
 
         @Volatile
         private var INSTANCE: KaboorDataStore? = null
@@ -42,6 +43,18 @@ class KaboorDataStore(context: Context) {
 
     private val Context.dataStore by preferencesDataStore(ConstDataStore.KABOOR_DATA_STORE)
     private val dataStore = context.dataStore
+
+    suspend fun login() {
+        dataStore.edit { preferences ->
+            preferences[STATE] = true
+        }
+    }
+
+    suspend fun logout() {
+        dataStore.edit { preferences ->
+            preferences[STATE] = false
+        }
+    }
 
     suspend fun saveToken(token: String) {
         dataStore.edit { preferences ->
